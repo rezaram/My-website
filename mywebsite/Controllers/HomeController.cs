013ee9b13ba68b8d4e28bb9b3fd2e69c8c1d0192
reza-ram <rezaramezannejad8632@gmail.com>
2024-05-14 13:33:47 +0330

    Added discounts section

--- mywebsite/Controllers/HomeController.cs
+++ mywebsite/Controllers/HomeController.cs
@@ -1,17 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using mywebsite.Models;
-using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace mywebsite.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
